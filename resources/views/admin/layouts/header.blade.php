2011807b87f787cc8959787c26ed7182b6e99069
Sachin Endait <endaitsachin11@gmail.com>
2023-12-24 20:00:49 +0530

    Modified asset files path

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -10,20 +10,20 @@
       <meta name="keywords" content="">
       <meta name="author" content="Phoenixcoded" />
       <!-- Favicon icon -->
-      <script src="{{ asset('assets/js/pcoded.min.js')}}"></script>
-      <script src="{{ asset('assets/js/jquery.min.js')}}"></script>
-      <script src="{{ asset('assets/js/common.js')}}"></script>
-      <script src="{{ asset('assets/js/Crypto.js')}}"></script>
-      <script src="{{ asset('assets/js/Encryption.js')}}"></script>
+      <script src="{{ asset('public/assets/js/pcoded.min.js')}}"></script>
+      <script src="{{ asset('public/assets/js/jquery.min.js')}}"></script>
+      <script src="{{ asset('public/assets/js/common.js')}}"></script>
+      <script src="{{ asset('public/assets/js/Crypto.js')}}"></script>
+      <script src="{{ asset('public/assets/js/Encryption.js')}}"></script>
       <script>
          var base_url = "{{url('/')}}";
          var _accessToken = "{{csrf_token()}}";
       </script>
 
-      <link rel="icon" href="{{ asset('assets/images/favicon.ico')}}" type="image/x-icon">
+      <link rel="icon" href="{{ asset('public/assets/images/favicon.ico')}}" type="image/x-icon">
       <!-- prism css -->
-      <link rel="stylesheet" href="{{ asset('assets/css/plugins/prism-coy.css')}}">
+      <link rel="stylesheet" href="{{ asset('public/assets/css/plugins/prism-coy.css')}}">
       <!-- vendor css -->
-      <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+      <link rel="stylesheet" href="{{ asset('public/assets/css/style.css')}}">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
    </head>
